96873d51d31edbc53ebcc0bf1614bbf220e98aab
Arushharsh <158551471+Arushharsh@users.noreply.github.com>
2026-02-23 13:33:04 +0530

    Update queueService.ts

--- src/backend/services/queueService.ts
+++ src/backend/services/queueService.ts
@@ -1,7 +1,12 @@
 import { Queue, Worker } from 'bullmq';
 import Redis from 'ioredis';
 
-const connection = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
+const connection = new Redis(
+  process.env.REDIS_URL || "redis://localhost:6379",
+  {
+    maxRetriesPerRequest: null,
+  }
+);;
 
 export const orderQueue = new Queue('order-lifecycle', { connection });
 
