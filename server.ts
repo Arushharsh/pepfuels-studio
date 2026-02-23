10afaec3d0e1c04d1adaead2ad65aded3f552090
Arushharsh <158551471+Arushharsh@users.noreply.github.com>
2026-02-23 12:58:18 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -50,7 +50,7 @@ async function startServer() {
   }
 
   app.listen(PORT, "0.0.0.0", () => {
-    console.log(`🚀 Pepfuels Server running on http://localhost:${PORT}`);
+    console.log(`🚀 Pepfuels Server running on port ${PORT}`);
   });
 }
 
